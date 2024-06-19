cf53b0c3e2da00fb398465cc1f0bebb9681c14d4
l.engelhardt <l.engelhardt@edu.letteverein.berlin>
2024-06-19 13:40:34 +0200

    "Added User foreign key and timestamp to Message model, and implemented __str__ method."

--- Chatify/chat/models.py
+++ Chatify/chat/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
-# Create your models here.
+class Message(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField()
+    timestamp = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f"{self.user.username}: {self.text} [{self.timestamp}]"
